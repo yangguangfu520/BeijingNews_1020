304375dab897f96a367b617c2dc7d226888aea09
atguiguafu <541433511@qq.com>
2017-02-05 10:52:14 +0800

    硅谷新闻项目是一款真实企业项目的改进，该项目主要功能包括实时浏览北京周边新闻，商城购物，在线支付等。项目中讲解的主流技术包括且不限于okHttp、Glide和Picasso 、RadioGroup+Fragment、消息推送、图片三级缓存、消息推送、html5与Android互调等技术。

---
+++ app/src/main/java/com/atguigu/beijingnews/WelcomeActivity.java
@@ -0,0 +1,82 @@
+package com.atguigu.beijingnews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private RelativeLayout activity_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
+        //三个动画：旋转动画，渐变动画，缩放动画
+        RotateAnimation ra = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000);//设置持续时间
+        ra.setFillAfter(true);//设置停留在旋转后的状态
+
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(2000);//设置持续时间
+        aa.setFillAfter(true);//设置停留在旋转后的状态
+
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(2000);//设置持续时间
+        sa.setFillAfter(true);//设置停留在旋转后的状态
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(aa);
+        set.addAnimation(ra);
+        set.addAnimation(sa);
+
+        //开始播放动画
+        activity_main.startAnimation(set);
+
+        //监听动画播放完成
+        set.setAnimationListener(new MyAnimationListener());
+
+
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener {
+
+        /**
+         * 当动画开始播放的时候回调
+         * @param animation
+         */
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        /**
+         * 当动画播放结束的时候回调
+         * @param animation
+         */
+        @Override
+       public void onAnimationEnd(Animation animation) {
+
+            Toast.makeText(WelcomeActivity.this, "动画播放完成", Toast.LENGTH_SHORT).show();
+        }
+
+        /**
+         * 当动画重复播放的时候回调
+         * @param animation
+         */
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+
+
+}
