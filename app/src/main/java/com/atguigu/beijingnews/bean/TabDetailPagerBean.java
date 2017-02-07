52ad399c9e007da09239536fca4450e23a225e05
atguiguafu <541433511@qq.com>
2017-02-07 09:58:55 +0800

    页签详情页面TabDetailPager布局和配置联网

---
+++ app/src/main/java/com/atguigu/beijingnews/bean/TabDetailPagerBean.java
@@ -0,0 +1,294 @@
+package com.atguigu.beijingnews.bean;
+
+import java.util.List;
+
+/**
+ * 作者：尚硅谷-杨光福 on 2017/2/7 09:55
+ * 微信：yangguangfu520
+ * QQ号：541433511
+ * 作用：xxxx
+ */
+public class TabDetailPagerBean {
+
+    /**
+     * data : {"countcommenturl":"/client/content/countComment/","more":"/static/api/news/10007/list_2.json","news":[{"id":147314,"title":"徒步登高欣赏京城第一片红叶","url":"/static/html/2015/10/20/714C6E50486D197B6F257C45.html","listimage":"/static/images/2015/10/20/3/1420927604KB07.jpg","pubdate":"2015-10-20 09:37","comment":true,"commenturl":"/client/user/newComment/147314","type":"news","commentlist":"/static/api/news/10012/14/147314/comment_1.json"},{"id":147313,"title":"重庆开通首条互联网公交专线","url":"/static/html/2015/10/20/704D6C52496C187A68227D43.html","listimage":"/static/images/2015/10/20/37/1420927604K49F.jpg","pubdate":"2015-10-20 09:31","comment":true,"commenturl":"/client/user/newComment/147313","type":"news","commentlist":"/static/api/news/10012/13/147313/comment_1.json"},{"id":147312,"title":"伦敦大本钟由于维修 要静音4个月","url":"/static/html/2015/10/20/764B68564F6A197B69237B44.html","listimage":"/static/images/2015/10/20/4/1420927604PBZA.jpg","pubdate":"2015-10-20 09:30","comment":true,"commenturl":"/client/user/newComment/147312","type":"news","commentlist":"/static/api/news/10012/12/147312/comment_1.json"},{"id":147267,"title":"别样南非：很野蛮但也很美丽","url":"/static/html/2015/10/19/714C6759486D1D7E662B7C46.html","listimage":"/static/images/2015/10/19/84/14209276048ADH.jpg","pubdate":"2015-10-19 11:03","comment":true,"commenturl":"/client/user/newComment/147267","type":"news","commentlist":"/static/api/news/10012/67/147267/comment_1.json"},{"id":147266,"title":"抄底游：这些城市便宜又好逛","url":"/static/html/2015/10/19/724F6F514A6F10736C217F44.html","listimage":"/static/images/2015/10/19/78/1420927604L1EF.jpg","pubdate":"2015-10-19 10:59","comment":true,"commenturl":"/client/user/newComment/147266","type":"news","commentlist":"/static/api/news/10012/66/147266/comment_1.json"},{"id":147149,"title":"北京公交开通5元赏红旅游专线","url":"/static/html/2015/10/16/714C68564C691A7A68277C48.html","listimage":"/static/images/2015/10/16/52/1420927604Y9DA.jpg","pubdate":"2015-10-16 10:44","comment":true,"commenturl":"/client/user/newComment/147149","type":"news","commentlist":"/static/api/news/10012/49/147149/comment_1.json"},{"id":147148,"title":"乘地铁不跷二郎腿 管宽了吗？","url":"/static/html/2015/10/16/704D6759406519796C237D48.html","listimage":"/static/images/2015/10/16/31/1420927604GCA5.jpg","pubdate":"2015-10-16 10:42","comment":true,"commenturl":"/client/user/newComment/147148","type":"news","commentlist":"/static/api/news/10012/48/147148/comment_1.json"},{"id":147146,"title":"耍家看过来！ 四川最美旅行时间表","url":"/static/html/2015/10/16/754866584C691D7D6C237843.html","listimage":"/static/images/2015/10/16/30/1420927604N7IG.jpg","pubdate":"2015-10-16 10:33","comment":true,"commenturl":"/client/user/newComment/147146","type":"news","commentlist":"/static/api/news/10012/46/147146/comment_1.json"},{"id":147124,"title":"海岛民宿成舟山旅游新亮点","url":"/static/html/2015/10/16/704D66584E6B19796A237148.html","listimage":"/static/images/2015/10/16/54/1420927604BQXZ.jpg","pubdate":"2015-10-16 09:48","comment":true,"commenturl":"/client/user/newComment/147124","type":"news","commentlist":"/static/api/news/10012/24/147124/comment_1.json"},{"id":147123,"title":"趁秋天还在！赶紧去赏秋","url":"/static/html/2015/10/16/764B68564A6F1B7B672E7B45.html","listimage":"/static/images/2015/10/16/42/1420927604BBFY.jpg","pubdate":"2015-10-16 09:42","comment":true,"commenturl":"/client/user/newComment/147123","type":"news","commentlist":"/static/api/news/10012/23/147123/comment_1.json"}],"title":"北京","topic":[],"topnews":[{"comment":true,"commentlist":"/static/api/news/10007/53/147253/comment_1.json","commenturl":"/client/user/newComment/147253","id":147253,"pubdate":"2015-10-19 07:18","title":"市教委：中高考英语试卷结构不变","topimage":"/static/images/2015/10/19/36/1053274969EORV.jpg","type":"news","url":"/static/html/2015/10/19/714C6E504A6F1B7869277C42.html"},{"comment":true,"commentlist":"/static/api/news/10007/54/147254/comment_1.json","commenturl":"/client/user/newComment/147254","id":147254,"pubdate":"2015-10-19 07:22","title":"警方披露\u201c二环十三郎\u201d审讯内容","topimage":"/static/images/2015/10/19/14/105235144895YS.jpg","type":"news","url":"/static/html/2015/10/19/724F6B554E6B1D7E68267C45.html"},{"id":147315,"title":"百年彭家寨 \u201c国保\u201d吊脚楼","topimage":"/static/images/2015/10/20/80/467283565SGJ.jpg","url":"/static/html/2015/10/20/754868564D681E7C68227840.html","pubdate":"2015-10-20 09:57","comment":true,"commenturl":"/client/user/newComment/147315","type":"news","commentlist":"/static/api/news/10012/15/147315/comment_1.json"},{"id":147274,"title":"年度最佳旅行地 让你砰然心动","topimage":"/static/images/2015/10/19/70/467283565C7N.jpg","url":"/static/html/2015/10/19/704D6C524D681D7E6D217D44.html","pubdate":"2015-10-19 11:42","comment":true,"commenturl":"/client/user/newComment/147274","type":"news","commentlist":"/static/api/news/10012/74/147274/comment_1.json"}]}
+     * retcode : 200
+     */
+
+    private DataEntity data;
+    private int retcode;
+
+    public DataEntity getData() {
+        return data;
+    }
+
+    public void setData(DataEntity data) {
+        this.data = data;
+    }
+
+    public int getRetcode() {
+        return retcode;
+    }
+
+    public void setRetcode(int retcode) {
+        this.retcode = retcode;
+    }
+
+    public static class DataEntity {
+        /**
+         * countcommenturl : /client/content/countComment/
+         * more : /static/api/news/10007/list_2.json
+         * news : [{"id":147314,"title":"徒步登高欣赏京城第一片红叶","url":"/static/html/2015/10/20/714C6E50486D197B6F257C45.html","listimage":"/static/images/2015/10/20/3/1420927604KB07.jpg","pubdate":"2015-10-20 09:37","comment":true,"commenturl":"/client/user/newComment/147314","type":"news","commentlist":"/static/api/news/10012/14/147314/comment_1.json"},{"id":147313,"title":"重庆开通首条互联网公交专线","url":"/static/html/2015/10/20/704D6C52496C187A68227D43.html","listimage":"/static/images/2015/10/20/37/1420927604K49F.jpg","pubdate":"2015-10-20 09:31","comment":true,"commenturl":"/client/user/newComment/147313","type":"news","commentlist":"/static/api/news/10012/13/147313/comment_1.json"},{"id":147312,"title":"伦敦大本钟由于维修 要静音4个月","url":"/static/html/2015/10/20/764B68564F6A197B69237B44.html","listimage":"/static/images/2015/10/20/4/1420927604PBZA.jpg","pubdate":"2015-10-20 09:30","comment":true,"commenturl":"/client/user/newComment/147312","type":"news","commentlist":"/static/api/news/10012/12/147312/comment_1.json"},{"id":147267,"title":"别样南非：很野蛮但也很美丽","url":"/static/html/2015/10/19/714C6759486D1D7E662B7C46.html","listimage":"/static/images/2015/10/19/84/14209276048ADH.jpg","pubdate":"2015-10-19 11:03","comment":true,"commenturl":"/client/user/newComment/147267","type":"news","commentlist":"/static/api/news/10012/67/147267/comment_1.json"},{"id":147266,"title":"抄底游：这些城市便宜又好逛","url":"/static/html/2015/10/19/724F6F514A6F10736C217F44.html","listimage":"/static/images/2015/10/19/78/1420927604L1EF.jpg","pubdate":"2015-10-19 10:59","comment":true,"commenturl":"/client/user/newComment/147266","type":"news","commentlist":"/static/api/news/10012/66/147266/comment_1.json"},{"id":147149,"title":"北京公交开通5元赏红旅游专线","url":"/static/html/2015/10/16/714C68564C691A7A68277C48.html","listimage":"/static/images/2015/10/16/52/1420927604Y9DA.jpg","pubdate":"2015-10-16 10:44","comment":true,"commenturl":"/client/user/newComment/147149","type":"news","commentlist":"/static/api/news/10012/49/147149/comment_1.json"},{"id":147148,"title":"乘地铁不跷二郎腿 管宽了吗？","url":"/static/html/2015/10/16/704D6759406519796C237D48.html","listimage":"/static/images/2015/10/16/31/1420927604GCA5.jpg","pubdate":"2015-10-16 10:42","comment":true,"commenturl":"/client/user/newComment/147148","type":"news","commentlist":"/static/api/news/10012/48/147148/comment_1.json"},{"id":147146,"title":"耍家看过来！ 四川最美旅行时间表","url":"/static/html/2015/10/16/754866584C691D7D6C237843.html","listimage":"/static/images/2015/10/16/30/1420927604N7IG.jpg","pubdate":"2015-10-16 10:33","comment":true,"commenturl":"/client/user/newComment/147146","type":"news","commentlist":"/static/api/news/10012/46/147146/comment_1.json"},{"id":147124,"title":"海岛民宿成舟山旅游新亮点","url":"/static/html/2015/10/16/704D66584E6B19796A237148.html","listimage":"/static/images/2015/10/16/54/1420927604BQXZ.jpg","pubdate":"2015-10-16 09:48","comment":true,"commenturl":"/client/user/newComment/147124","type":"news","commentlist":"/static/api/news/10012/24/147124/comment_1.json"},{"id":147123,"title":"趁秋天还在！赶紧去赏秋","url":"/static/html/2015/10/16/764B68564A6F1B7B672E7B45.html","listimage":"/static/images/2015/10/16/42/1420927604BBFY.jpg","pubdate":"2015-10-16 09:42","comment":true,"commenturl":"/client/user/newComment/147123","type":"news","commentlist":"/static/api/news/10012/23/147123/comment_1.json"}]
+         * title : 北京
+         * topic : []
+         * topnews : [{"comment":true,"commentlist":"/static/api/news/10007/53/147253/comment_1.json","commenturl":"/client/user/newComment/147253","id":147253,"pubdate":"2015-10-19 07:18","title":"市教委：中高考英语试卷结构不变","topimage":"/static/images/2015/10/19/36/1053274969EORV.jpg","type":"news","url":"/static/html/2015/10/19/714C6E504A6F1B7869277C42.html"},{"comment":true,"commentlist":"/static/api/news/10007/54/147254/comment_1.json","commenturl":"/client/user/newComment/147254","id":147254,"pubdate":"2015-10-19 07:22","title":"警方披露\u201c二环十三郎\u201d审讯内容","topimage":"/static/images/2015/10/19/14/105235144895YS.jpg","type":"news","url":"/static/html/2015/10/19/724F6B554E6B1D7E68267C45.html"},{"id":147315,"title":"百年彭家寨 \u201c国保\u201d吊脚楼","topimage":"/static/images/2015/10/20/80/467283565SGJ.jpg","url":"/static/html/2015/10/20/754868564D681E7C68227840.html","pubdate":"2015-10-20 09:57","comment":true,"commenturl":"/client/user/newComment/147315","type":"news","commentlist":"/static/api/news/10012/15/147315/comment_1.json"},{"id":147274,"title":"年度最佳旅行地 让你砰然心动","topimage":"/static/images/2015/10/19/70/467283565C7N.jpg","url":"/static/html/2015/10/19/704D6C524D681D7E6D217D44.html","pubdate":"2015-10-19 11:42","comment":true,"commenturl":"/client/user/newComment/147274","type":"news","commentlist":"/static/api/news/10012/74/147274/comment_1.json"}]
+         */
+
+        private String countcommenturl;
+        private String more;
+        private String title;
+        private List<NewsEntity> news;
+        private List<?> topic;
+        private List<TopnewsEntity> topnews;
+
+        public String getCountcommenturl() {
+            return countcommenturl;
+        }
+
+        public void setCountcommenturl(String countcommenturl) {
+            this.countcommenturl = countcommenturl;
+        }
+
+        public String getMore() {
+            return more;
+        }
+
+        public void setMore(String more) {
+            this.more = more;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<NewsEntity> getNews() {
+            return news;
+        }
+
+        public void setNews(List<NewsEntity> news) {
+            this.news = news;
+        }
+
+        public List<?> getTopic() {
+            return topic;
+        }
+
+        public void setTopic(List<?> topic) {
+            this.topic = topic;
+        }
+
+        public List<TopnewsEntity> getTopnews() {
+            return topnews;
+        }
+
+        public void setTopnews(List<TopnewsEntity> topnews) {
+            this.topnews = topnews;
+        }
+
+        public static class NewsEntity {
+            /**
+             * id : 147314
+             * title : 徒步登高欣赏京城第一片红叶
+             * url : /static/html/2015/10/20/714C6E50486D197B6F257C45.html
+             * listimage : /static/images/2015/10/20/3/1420927604KB07.jpg
+             * pubdate : 2015-10-20 09:37
+             * comment : true
+             * commenturl : /client/user/newComment/147314
+             * type : news
+             * commentlist : /static/api/news/10012/14/147314/comment_1.json
+             */
+
+            private int id;
+            private String title;
+            private String url;
+            private String listimage;
+            private String pubdate;
+            private boolean comment;
+            private String commenturl;
+            private String type;
+            private String commentlist;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public String getListimage() {
+                return listimage;
+            }
+
+            public void setListimage(String listimage) {
+                this.listimage = listimage;
+            }
+
+            public String getPubdate() {
+                return pubdate;
+            }
+
+            public void setPubdate(String pubdate) {
+                this.pubdate = pubdate;
+            }
+
+            public boolean isComment() {
+                return comment;
+            }
+
+            public void setComment(boolean comment) {
+                this.comment = comment;
+            }
+
+            public String getCommenturl() {
+                return commenturl;
+            }
+
+            public void setCommenturl(String commenturl) {
+                this.commenturl = commenturl;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getCommentlist() {
+                return commentlist;
+            }
+
+            public void setCommentlist(String commentlist) {
+                this.commentlist = commentlist;
+            }
+        }
+
+        public static class TopnewsEntity {
+            /**
+             * comment : true
+             * commentlist : /static/api/news/10007/53/147253/comment_1.json
+             * commenturl : /client/user/newComment/147253
+             * id : 147253
+             * pubdate : 2015-10-19 07:18
+             * title : 市教委：中高考英语试卷结构不变
+             * topimage : /static/images/2015/10/19/36/1053274969EORV.jpg
+             * type : news
+             * url : /static/html/2015/10/19/714C6E504A6F1B7869277C42.html
+             */
+
+            private boolean comment;
+            private String commentlist;
+            private String commenturl;
+            private int id;
+            private String pubdate;
+            private String title;
+            private String topimage;
+            private String type;
+            private String url;
+
+            public boolean isComment() {
+                return comment;
+            }
+
+            public void setComment(boolean comment) {
+                this.comment = comment;
+            }
+
+            public String getCommentlist() {
+                return commentlist;
+            }
+
+            public void setCommentlist(String commentlist) {
+                this.commentlist = commentlist;
+            }
+
+            public String getCommenturl() {
+                return commenturl;
+            }
+
+            public void setCommenturl(String commenturl) {
+                this.commenturl = commenturl;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getPubdate() {
+                return pubdate;
+            }
+
+            public void setPubdate(String pubdate) {
+                this.pubdate = pubdate;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getTopimage() {
+                return topimage;
+            }
+
+            public void setTopimage(String topimage) {
+                this.topimage = topimage;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+    }
+}
--- app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
@@ -1,13 +1,23 @@
 package com.atguigu.beijingnews.detailpager;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.atguigu.beijingnews.R;
 import com.atguigu.beijingnews.base.MenuDetailBasePager;
 import com.atguigu.beijingnews.bean.NewsCenterBean;
+import com.atguigu.beijingnews.bean.TabDetailPagerBean;
+import com.atguigu.beijingnews.utils.Constants;
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * 作者：尚硅谷-杨光福 on 2017/2/6 15:26
@@ -17,27 +27,64 @@ import com.atguigu.beijingnews.bean.NewsCenterBean;
  */
 public class TabDetailPager extends MenuDetailBasePager {
     private final NewsCenterBean.DataBean.ChildrenBean childrenBean;
+    @InjectView(R.id.listview)
+    ListView listview;
+    private String url;
 
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
         this.childrenBean = childrenBean;
     }
-    private TextView textView;
+
     @Override
     public View initView() {
         //图组详情页面的视图
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.tab_detail_pager, null);
+        ButterKnife.inject(this,view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
+        url = Constants.BASE_URL + childrenBean.getUrl();
+        Log.e("TAG","TabDetailPager--url=="+url);
         //设置数据
-        textView.setText(childrenBean.getTitle());
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(url);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("TAG","请求数据成功==TabDetailPager=="+childrenBean.getTitle());
+                processData(result);
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG","请求数据失败==TabDetailPager=="+ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    private void processData(String json) {
+        TabDetailPagerBean pagerBean = new Gson().fromJson(json,TabDetailPagerBean.class);
+
+        Log.e("TAG","数据解析成功==TabDetailPager=="+pagerBean.getData().getNews().get(0).getTitle());
+
     }
 
 }
