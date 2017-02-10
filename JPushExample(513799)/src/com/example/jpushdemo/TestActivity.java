b2d391410e5aa901a6017394382ae3f7e7d6943f
atguiguafu <541433511@qq.com>
2017-02-10 15:01:22 +0800

    极光推送集成到项目中

---
+++ JPushExample(513799)/src/com/example/jpushdemo/TestActivity.java
@@ -0,0 +1,27 @@
+package com.example.jpushdemo;
+
+import cn.jpush.android.api.JPushInterface;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("用户自定义打开的Activity");
+        Intent intent = getIntent();
+        if (null != intent) {
+	        Bundle bundle = getIntent().getExtras();
+	        String title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
+	        String content = bundle.getString(JPushInterface.EXTRA_ALERT);
+	        tv.setText("Title : " + title + "  " + "Content : " + content);
+        }
+        addContentView(tv, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+    }
+
+}
--- app/src/main/java/com/atguigu/beijingnews/MyApplication.java
+++ app/src/main/java/com/atguigu/beijingnews/MyApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
+
+
 /**
  * 作者：尚硅谷-杨光福 on 2017/2/6 10:07
  * 微信：yangguangfu520
@@ -16,5 +20,8 @@ public class MyApplication extends Application {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
